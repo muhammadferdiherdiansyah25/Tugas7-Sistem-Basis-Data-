d1664ce3330576205fb7642e131401b069db32a4
muhammadferdiherdiansyah25 <101733752+muhammadferdiherdiansyah25@users.noreply.github.com>
2022-06-18 22:23:01 +0700

    Add files via upload

---
+++ Pasien.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Menampilkan Data Dari Database PHP </title>
+    <style>
+        table,tr,td {
+            border: 1px solid black;
+        }
+        thead {
+            background-color: #cccddd;
+        }
+    </style>
+</head>
+<body>
+    <h1 style>Sistem Informasi Klinik</h1>
+    <table>
+        <thead>
+            <tr>
+                <td>No</td>
+				<td>Id Pasien</td>
+                <td>Nama Pasien</td>
+                <td>Jenis Kelamin</td>
+                <td>Umur</td>                
+            </tr>
+        </thead>
+        <?php
+        include "koneksi.php";
+        $no = 1;
+        $query = mysqli_query($conn, 'SELECT * FROM Pasien');
+        while ($data = mysqli_fetch_array($query)) {
+        ?>
+            <tr>
+                <td><?php echo $no++ ?></td>
+				<td><?php echo $data['id_pasien'] ?></td>
+                <td><?php echo $data['nama_pasien'] ?></td>
+                <td><?php echo $data['jenis_kelamin'] ?></td>
+                <td><?php echo $data['umur'] ?></td>
+            </tr>
+        <?php } ?>
+    </table>
+</body>
